c72ead64895d4dd3acacb6fa346848310fbcacbd
Goran <gospaso@gmail.com>
2023-10-03 22:01:27 -0400

    nvim search files, grep hidden

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -256,6 +256,16 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+    live_grep = {
+      additional_args = function(opts)
+        return { "--hidden" }
+      end
+    },
+  },
 }
 
 -- Enable telescope fzf native, if installed
