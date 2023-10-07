f6040aedc838a2f8b571f7eadb8f31c0b0fd8a9b
Goran <gospaso@gmail.com>
2023-10-07 19:20:37 -0400

    nvim minor bindings

--- .config/nvim/lua/custom/plugins/remap.lua
+++ .config/nvim/lua/custom/plugins/remap.lua
@@ -1,19 +1,17 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- move highlighted rows
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<A-j>", ":call append(line('.'), '')<CR>")
-vim.keymap.set("n", "<A-k>", ":call append(line('.')-1, '')<CR>")
-
--- vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-down>", "<C-E>", { noremap = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-up>", "<C-Y>", { noremap = true })
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<A-j>", ":call append(line('.'), '')<CR>")
+vim.keymap.set("n", "<A-k>", ":call append(line('.')-1, '')<CR>")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
@@ -26,7 +24,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -39,7 +37,5 @@ vim.keymap.set("n",
     { desc = '[S]each and Repla[s]e' }
 )
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
 
 return {}
